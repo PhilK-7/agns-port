1473eadaea4fd79d112f92a5a138a4c989a07af2
Philipp K <philippkubon@protonmail.com>
2021-06-23 20:40:10 +0200

    Finish building OpenFace

--- agns-py/src/face_nets.py
+++ agns-py/src/face_nets.py
@@ -44,6 +44,10 @@ def get_original_vgg_model():
 
 
 class LocalResponseNormalization(tf.keras.layers.Layer):
+    """
+    The Local Response Normalization layer. Normalizes high frequency features with radial masks.
+    Applies the parameters needed in OpenFace NN4.small2.v1.
+    """
     def __init__(self):
         super(LocalResponseNormalization, self).__init__()
 
@@ -51,15 +55,27 @@ class LocalResponseNormalization(tf.keras.layers.Layer):
         return tf.nn.local_response_normalization(inputs, alpha=1e-4, beta=0.75)
 
 
-class LPPooling(tf.keras.layers.Layer):
+class L2Pooling(tf.keras.layers.Layer):
+    """
+    The L2 Pooling layer. Computes a kind of Euclidean norm using average pooling.
+    Uses pooling size 3, as needed in OpenFace NN4.small2.v1.
+    """
     def __init__(self):
-        super(LPPooling, self).__init__()
+        super(L2Pooling, self).__init__()
 
     def call(self, inputs, **kwargs):
-        pass
+        x = tf.math.square(inputs)
+        x = tf.keras.layers.AvgPool2D((3, 3), (1, 1), padding='same')(x)
+        x = tf.math.multiply(x, 9)
+        x = tf.math.sqrt(x)
+
+        return x
 
 
 class L2Normalization(tf.keras.layers.Layer):
+    """
+    The L2 Normalization layer. Just computes the L2 norm of its input.
+    """
     def __init__(self):
         super(L2Normalization, self).__init__()
 
@@ -70,8 +86,7 @@ class L2Normalization(tf.keras.layers.Layer):
 
 class InceptionModule(tf.keras.layers.Layer):
     # TODO doc
-    # TODO L2 pooling?
-    def __init__(self, conv_output_sizes, reduce_sizes, name):
+    def __init__(self, conv_output_sizes, reduce_sizes, name, use_l2_pooling=False):
         super(InceptionModule, self).__init__(name=name)
 
         assert len(conv_output_sizes) >= 1
@@ -85,29 +100,50 @@ class InceptionModule(tf.keras.layers.Layer):
         if len(conv_output_sizes) == 1:
             self.shift = 1  # adjust reduce + pool indices if necessary
         self.reduce_conv_out = tf.keras.layers.Conv2D(self.rs[3 - self.shift], (1, 1), padding='same')
+        self.rco_bn = tf.keras.layers.BatchNormalization(axis=3)
         self.reduce_3 = tf.keras.layers.Conv2D(self.rs[0], (1, 1), padding='same')
+        self.r3_bn = tf.keras.layers.BatchNormalization(axis=3)
         self.out_3 = tf.keras.layers.Conv2D(self.cos[0], (3, 3), padding='same')
+        self.o3_bn = tf.keras.layers.BatchNormalization(axis=3)
         if self.shift == 0:  # only add these layers for one variant
             self.reduce_5 = tf.keras.layers.Conv2D(self.rs[1], (1, 1), padding='same')
+            self.r5_bn = tf.keras.layers.BatchNormalization(axis=3)
             self.out_5 = tf.keras.layers.Conv2D(self.cos[1], (5, 5), padding='same')
-        self.pool = tf.keras.layers.MaxPool2D((3, 3), (1, 1), 'same')
+            self.o5_bn = tf.keras.layers.BatchNormalization(axis=3)
+        if use_l2_pooling:
+            self.pool = L2Pooling()
+        else:
+            self.pool = tf.keras.layers.MaxPool2D((3, 3), (1, 1), 'same')
         self.pool_out = tf.keras.layers.Conv2D(self.rs[2 - self.shift], (1, 1), padding='same')
+        self.po_bn = tf.keras.layers.BatchNormalization(axis=3)
 
     def call(self, inputs, **kwargs):
         # only reduction part
         p1 = self.reduce_conv_out(inputs)
+        p1 = self.rco_bn(p1)
+        p1 = tf.keras.layers.ReLU()(p1)
         # 3x3 convolution part
         p2 = self.reduce_3(inputs)
+        p2 = self.r3_bn(p2)
+        p2 = tf.keras.layers.ReLU()(p2)
         p2 = self.out_3(p2)
+        p2 = self.o3_bn(p2)
+        p2 = tf.keras.layers.ReLU()(p2)
         # 5x5 convolution part (one of two variants)
         if self.shift == 0:
             p3 = self.reduce_5(inputs)
+            p3 = self.r5_bn(p3)
+            p3 = tf.keras.layers.ReLU()(p3)
             p3 = self.out_5(p3)
+            p3 = self.o5_bn(p3)
+            p3 = tf.keras.layers.ReLU()(p3)
         else:
             p3 = None
         # pooling part
         p4 = self.pool(inputs)  # pooling without reducing filters
         p4 = self.pool_out(p4)
+        p4 = self.po_bn(p4)
+        p4 = tf.keras.layers.ReLU()(p4)
 
         concat = tf.keras.layers.Concatenate(axis=3)
 
@@ -115,6 +151,7 @@ class InceptionModule(tf.keras.layers.Layer):
 
 
 class InceptionModuleShrink(tf.keras.layers.Layer):
+    # TODO doc
     def __init__(self, conv_output_sizes, reduce_sizes, name):
         super(InceptionModuleShrink, self).__init__(name=name)
 
@@ -122,42 +159,43 @@ class InceptionModuleShrink(tf.keras.layers.Layer):
         self.cos = conv_output_sizes
         self.rs = reduce_sizes
 
+        self.reduce3 = tf.keras.layers.Conv2D(self.rs[0], (1, 1), padding='same')
+        self.r3_bn = tf.keras.layers.BatchNormalization(axis=3)
+        self.conv3 = tf.keras.layers.Conv2D(self.cos[0], (3, 3), (2, 2), 'same')
+        self.c3_bn = tf.keras.layers.BatchNormalization(axis=3)
+        self.reduce5 = tf.keras.layers.Conv2D(self.rs[1], (1, 1), padding='same')
+        self.r5_bn = tf.keras.layers.BatchNormalization(axis=3)
+        self.conv5 = tf.keras.layers.Conv2D(self.cos[1], (5, 5), (2, 2), 'same')
+        self.c5_bn = tf.keras.layers.BatchNormalization(axis=3)
+        self.pool = tf.keras.layers.MaxPool2D((3, 3), (2, 2), padding='same')
+
     def call(self, inputs, **kwargs):
         # 3x3 convolution part
-        p1 = tf.keras.layers.Conv2D(self.rs[0], (1, 1), padding='same')(inputs)
-        p1_a = tf.keras.layers.Conv2D(self.cos[0], (3, 3), (2, 2), 'same')(p1)
-        p1_b = tf.keras.layers.Conv2D(self.cos[0], (3, 3), (2, 2), 'same')(p1)
+        p1 = self.reduce3(inputs)
+        p1 = self.r3_bn(p1)
+        p1 = tf.keras.layers.ReLU()(p1)
+        p1 = self.conv3(p1)
+        p1 = self.c3_bn(p1)
+        p1 = tf.keras.layers.ReLU()(p1)
         # 5x5 convolution part
-        p2 = tf.keras.layers.Conv2D(self.rs[1], (1, 1), padding='same')(inputs)
-        p2_a = tf.keras.layers.Conv2D(self.cos[1], (5, 5), (2, 2), 'same')(p2)
-        p2_b = tf.keras.layers.Conv2D(self.cos[1], (5, 5), (2, 2), 'same')(p2)
+        p2 = self.reduce5(inputs)
+        p2 = self.r5_bn(p2)
+        p2 = tf.keras.layers.ReLU()(p2)
+        p2 = self.conv5(p2)
+        p2 = self.c5_bn(p2)
+        p2 = tf.keras.layers.ReLU()(p2)
         # pooling part
-        convs_out = tf.keras.layers.Concatenate(axis=3)([p1_a, p1_b, p2_a, p2_b])
-        pool = tf.keras.layers.MaxPool2D((3, 3), (2, 2))(convs_out)
+        pool = self.pool(inputs)
 
-        return pool
-
-
-def get_inception_module_output(inp, conv_output_sizes, reduce_sizes):
-    """
-    Builds an Inception module with given properties.
-    A 4D tensor input is given, and the result is the concatenation of all inception module pathway outputs.
-
-    :param inp: the Inception module´s input, a 4D tensor (batch_size, , , )
-
-    """
+        return tf.keras.layers.Concatenate(axis=3)([p1, p2, pool])
 
 
 def build_openface_model():
     """
-    Builds the nn4.small2 OpenFace model, with about 3.74M parameters (3733968)
+    Builds the nn4.small2 OpenFace model, with about 3.74M trainable parameters (3733968)
     (exact model type was inferred from paper's parameter count).
     The model's output is a 128-sphere.
     """
-    '''with tf.keras.utils.CustomObjectScope({'tf': tf}):
-        model = tf.keras.models.load_model('../nn4.small2.v1.h5')
-        model.summary()
-        return model'''
 
     # input part
     inp = tf.keras.layers.Input((96, 96, 3))  # input is (aligned) RBG image pf 96x96
@@ -183,18 +221,19 @@ def build_openface_model():
     x = InceptionModule([128, 32], [96, 16, 32, 64], 'Inception_3a')(x)
 
     # Inception 3b (output size 12x12 x 320)
-    x = InceptionModule([128, 64], [96, 32, 64, 64], 'Inception_3b')(x)
+    x = InceptionModule([128, 64], [96, 32, 64, 64], 'Inception_3b', True)(x)
 
     # Inception 3c (output size 6x6 x 640)
-    #x = InceptionModuleShrink([256, 64], [128, 32], 'Inception_3c')(x)
+    x = InceptionModuleShrink([256, 64], [128, 32], 'Inception_3c')(x)
 
     # Inception 4a (output size 6x6 x 640)
-    x = InceptionModule([192, 64], [96, 32, 128, 256], 'Inception_4a')(x)
+    x = InceptionModule([192, 64], [96, 32, 128, 256], 'Inception_4a', True)(x)
 
     # Inception 4e (output size 3x3 x 1024)
+    x = InceptionModuleShrink([256, 128], [160, 64], 'Inception_4e')(x)
 
     # Inception 5a (output size 3x3 x 736)
-    x = InceptionModule([384], [96, 96, 256], 'Inception_5a')(x)
+    x = InceptionModule([384], [96, 96, 256], 'Inception_5a', True)(x)
 
     # Inception 5b (output size 3x3 x 736)
     x = InceptionModule([384], [96, 96, 256], 'Inception_5b')(x)
