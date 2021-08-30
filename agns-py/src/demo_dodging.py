e8869d3a75ff1a106dfcda33eccf87cd710f480b
Philipp K <philippkubon@protonmail.com>
2021-08-30 17:05:33 +0200

    Add impersonation demo

--- agns-py/src/demo_dodging.py
+++ agns-py/src/demo_dodging.py
@@ -6,22 +6,21 @@ if __name__ == '__main__':
     dap = setup_params(True, 4)
 
     # some hyperparameters
-    ep = 100
+    ep = 100  # TODO 1 ?!
     lr = 5e-5
     kappa = 0.25
     stop_prob = 0.01
     bs = 32
 
-    # load models and set more values
+    # set paths and more values
     target = 19
     target_path = 'pubfig/dataset_aligned/Danny_Devito/aligned/'  # relative to 'data'
     img_size = (224, 224)  # input size for VGG
     mask_path = 'eyeglasses/eyeglasses_mask_6percent.png'
-    print('Loading models...')
     fn_path = '../saved-models/vgg_143.h5'
     g_path = '../saved-models/gweights'
     d_path = '../saved-models/dweights'
 
-    # call attack configuration
+    # execute dodging attack
     attacks.execute_attack(dap, target_path, mask_path, img_size, g_path, d_path, fn_path, True, ep, lr, kappa,
                            stop_prob, bs, target, True, True)
---
+++ agns-py/src/demo_impersonation.py
@@ -0,0 +1,24 @@
+from setup import setup_params
+from attacks import execute_attack
+
+# subject n° 1 digital impersonation attack -vs- OpenFace 10
+if __name__ == '__main__':
+    dap = setup_params(True, 0)
+
+    ep = 100  # TODO 1 ?!
+    stop_prob = 0.924
+    kappa = 0.25
+    lr = 5e-5
+
+    # set values
+    target = 1
+    target_path = 'pubfig/dataset_aligned_10/Barack_Obama/aligned/'
+    img_size = (96, 96)  # input size for OpenFace
+    mask_path = 'eyeglasses/eyeglasses_mask_6percent.png'
+    fn_path = '../saved-models/of10.h5'
+    g_path = '../saved-models/gweights'
+    d_path = '../saved-models/dweights'
+
+    # execute impersonation attack
+    execute_attack(dap, target_path, mask_path, img_size, g_path, d_path, fn_path, False, ep, lr, kappa, stop_prob, 32,
+                   target, False, False)
